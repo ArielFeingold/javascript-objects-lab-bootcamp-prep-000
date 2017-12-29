cbadbdea2ff58063849f49fbd87aa6fbf45f9ff2
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-29 18:03:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes= {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj2 = Object.assign({}, object, {[key]: [value]});
+  return obj2
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj2 = [];
+  Object.assign(obj2, object, {[key]: value})
+  return obj2
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+   var obj2 = [];
+  Object.assign(obj2, object, delete.object.key)
+  return obj2
+}
